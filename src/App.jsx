f0d8529e21903fbb6523dbfbc1fb3472a72b116c
yudagn <yudagn@gmail.com>
2024-09-18 11:44:43 -0400

    update base for prod

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./shared-components/ScrollToTop";
 import HomePage from "./pages/home";
 
+const basename = import.meta.env.MODE === "production" ? "/Portfolio" : "";
+
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/Portfolio/" : "/", // Apply base only in production
   plugins: [react()],
-});
+}));
